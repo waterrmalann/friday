bb352d6188142b6a78f791fd8874e3b535d25f34
Alan <56948680+waterrmalann@users.noreply.github.com>
2022-09-28 22:06:30 +0530

    🎲 Added support for variable sided dice

--- skills/active/Skill_RollDice/main.py
+++ skills/active/Skill_RollDice/main.py
@@ -16,28 +16,43 @@ class Skill_RollDice(SBRE.Skill):
                 {'a', },
                 ("dice", "die")
             ])
-        ]
 
     def process(self, message:Message):
+        
         reply = SentenceConstructor()
-        reply.add_syntax([
-            "I rolled",
-            ("a dice", "a die"),
-            ("and got", "and its", ", its", ", got"),
-            "{}"
-        ])
-        reply.add_syntax([
-            "{}",
-            "it is"
-        ])
-        reply.add_syntax([
-            "I got",
-            "{}"
-        ])
 
-        # Roll a six-sided die.
-        result = random.randint(1, 6)
-        out = reply.generate(result)
+        # Check if a natural number is in the string.
+        pe = PatternExpression([2])
+        if pe.match(message):
+            num = pe.extract(message)[MessageTokens.NaturalNumber]
+            result = random.randint(1, num)
+
+            reply.add_syntax([
+                "I rolled a {number}-sided die",
+                ("and got", "and its", ", its", ", got"),
+                "{result}"
+            ])
+
+            out = reply.generate(result=result, number=num)
+        else:
+            reply.add_syntax([
+                "I rolled",
+                ("a dice", "a die"),
+                ("and got", "and its", ", its", ", got"),
+                "{}"
+            ])
+            reply.add_syntax([
+                "{}",
+                "it is"
+            ])
+            reply.add_syntax([
+                "I got",
+                "{}"
+            ])
+
+            # Roll a six-sided die.
+            result = random.randint(1, 6)
+            out = reply.generate(result)
 
         return Response('OK', out)
 
